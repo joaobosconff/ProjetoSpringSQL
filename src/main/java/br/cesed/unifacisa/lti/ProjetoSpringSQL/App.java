8d19618f00f2963b0ccb55bacd87d499e8762e22
João Bosco <joaobosconff03@gmail.com>
2018-11-09 13:27:50 -0300

    projeto criado

---
+++ src/main/java/br/cesed/unifacisa/lti/ProjetoSpringSQL/App.java
@@ -0,0 +1,13 @@
+package br.cesed.unifacisa.lti.ProjetoSpringSQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
